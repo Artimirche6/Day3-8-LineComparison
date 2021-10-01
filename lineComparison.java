a1fdc142dd642d7c8f4325e099692103be02ea1d
Artimirche6 <artimirche6@gmail.com>
2021-10-02 01:15:37 +0530

    [arti] Check equality of two using compareTo

--- lineComparison.java
+++ lineComparison.java
@@ -11,15 +11,27 @@ public class lineComparison
 		System.out.println("Line Length for x1, x2, y1, y2 : "+len1);
 		System.out.println("Line Length for x11, x22, y11, y22 : "+len2);
 		
-		if (len1 == len2) 
-		{
-			System.out.println("Lines are equal");
-		}
-		else
-		{
-			System.out.println("Lines are not equal");
-		}
+		Integer i1 = new Integer(len1);
+		Integer i2 = new Integer(len2);
 		
+		int result = i1.compareTo(i2);
+		
+		System.out.println("Compare: "+i1.compareTo(i2));
+		
+		System.out.println("Equals : "+i1.equals(i2));
+		
+		if(result > 0)
+		{  
+			System.out.println("Length1 : " +i1+ " is greater than Length2 : "+i2);  
+		} 
+		else if(result < 0) 
+		{  
+			System.out.println("Length2 : " +i2+ " is greater than Length1 : "+i1);    
+		} 
+		else 
+		{  
+			System.out.println("Lines are Equals");  
+		}  
 	}
 
 }
