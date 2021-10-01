d8f91e647d9680ea149a271a356b76bb1ae55ab2
Artimirche6 <artimirche6@gmail.com>
2021-10-02 00:19:39 +0530

    [arti] Check equality of two lines are the equal or not

--- lineComparison.java
+++ lineComparison.java
@@ -2,11 +2,24 @@ public class lineComparison
 {
 	public static void main(String[] args)
 	{
-		int x1 = 5, x2 = 10;
-		int y1 = 6, y2 = 12;
+		int x1 = 5, x2 = 10, y1 = 6, y2 = 12;
+		int x11 = 2, x22 = 4, y11 = 5, y22 = 14;
 		
-		int length = (int) Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-
-		System.out.println("Line Length ===> "+length);
+		int len1 = (int) Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		int len2 = (int) Math.sqrt((Math.pow((x22-x11),2)) + (Math.pow((y22-y11),2)));
+		
+		System.out.println("Line Length for x1, x2, y1, y2 : "+len1);
+		System.out.println("Line Length for x11, x22, y11, y22 : "+len2);
+		
+		if (len1 == len2) 
+		{
+			System.out.println("Lines are equal");
+		}
+		else
+		{
+			System.out.println("Lines are not equal");
+		}
+		
 	}
+
 }
